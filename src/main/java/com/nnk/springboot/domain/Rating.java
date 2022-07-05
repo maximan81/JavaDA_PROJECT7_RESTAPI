1e233a70e565da3eb50ce27eeefb959915094e2c
Eric Maximan <ericmaximan@gmail.com>
2022-07-05 10:51:54 +0200

    domain implement

--- src/main/java/com/nnk/springboot/domain/Rating.java
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,36 @@
 package com.nnk.springboot.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
+@Data
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String moodysRating;
+
+  private String sandPRating;
+
+  private String fitchRating;
+
+  public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
+    this.moodysRating = moodysRating;
+    this.sandPRating = sandPRating;
+    this.fitchRating = fitchRating;
+    this.orderNumber = orderNumber;
+  }
+
+  private Integer orderNumber;
+
+
 }
